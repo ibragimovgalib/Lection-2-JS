d3bd2a27d432c711fc4f1b780caf651de1917985
ibragimovgalib <galibiavi4@gmail.com>
2023-03-17 20:51:06 +0300

    home task 2

--- HW 03/main.js
+++ HW 03/main.js
@@ -19,13 +19,21 @@ function minAndMax(a, b, c) {
 }
 console.log(minAndMax(20, 12, 2));
 
-// Task 4
+// Task 4_______________________________________
 function nextEvenNum(n){
     return Math.floor(n/2)*2+2
 }
 console.log(nextEvenNum(5));
 
-// Task 5___________?
+// Task 5________________________________________
+function sequenceNum(a,b,c){
+  s=0
+  if(a==c && a!=b) s=2
+  else if(a==b && a!=c) s=3
+  else s=1
+  return s
+}
+console.log(sequenceNum(3,2,3));
 
 // Task 6__________?
 
@@ -53,3 +61,11 @@ function sumOfinequalities(n,a){
 console.log(sumOfinequalities(24,7));
 
 // Task 10_____________?
+function threeGigitNum(n){
+  let a1=Math.floor(n/100)
+  let a2=Math.floor(n/10)%10
+  let a3=n%10
+  if(a1*a2*a3>=100) return true
+  else return false 
+}
+console.log(threeGigitNum(111));
--- class work/lection 2/main.js
+++ class work/lection 2/main.js
@@ -59,12 +59,51 @@
 
 // Closure
 
-// 1___________?
+// 1___________
+{
+    function constt(s){
+    return function(){
+        return s
+    }
+}
+console.log(constt("apple")());
+}
+
 
 // 2
-{
-    function adds1(n){
-        return n+1
+function adds1(n){
+    return function(){
+        return Math.floor(n)+1
     }
-    console.log(adds1(5));
 }
+console.log(adds1(5.7)());
+
+// Returning an "Add" Function
+function add(n1){
+    return function(n2){
+        return n1+n2
+    }
+}
+let ss=add(10)
+console.log(ss(20));
+
+// All About Anonymous Functions: Adding Suffixes
+function add_ly(str){
+    return function(ly){
+        return str+ly
+    }
+}
+console.log(add_ly("total")("ly"));
+console.log(add_ly("ruth")("less"));
+
+// Funtion Times 3
+function product(a1,b1){
+    return function(a2,b2){
+        return function(a3,b3){
+            return a1*a2*a3+b1*b2*b3
+        }
+    }
+}
+console.log(product(1,2)(1,1)(2,3));
+console.log(product(10,2)(5,0)(2,3));
+
